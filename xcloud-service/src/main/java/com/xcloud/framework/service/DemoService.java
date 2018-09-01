fb811e5b8b83821c40795c7d852acd06bba58716
fuminluo <474985045@qq.com>
2018-09-01 19:14:10 +0800

    修改代码

---
+++ xcloud-service/src/main/java/com/xcloud/framework/service/DemoService.java
@@ -0,0 +1,15 @@
+package com.xcloud.framework.service;
+
+import com.xcloud.framework.entity.User;
+
+/**
+ * service demo
+ *
+ * @Author administered
+ * @Description
+ * @Date 2018/9/1 14:56
+ **/
+public interface DemoService {
+
+    User save();
+}
---
+++ xcloud-service/src/main/java/com/xcloud/framework/service/impl/DemoServiceImpl.java
@@ -0,0 +1,22 @@
+package com.xcloud.framework.service.impl;
+
+import com.xcloud.framework.entity.User;
+import com.xcloud.framework.service.DemoService;
+import org.springframework.stereotype.Service;
+
+/**
+ * DemoServiceImpl 类
+ *
+ * @Author administered
+ * @Description
+ * @Date 2018/9/1 14:57
+ **/
+@Service
+public class DemoServiceImpl implements DemoService {
+
+
+    @Override
+    public User save() {
+        return null;
+    }
+}
